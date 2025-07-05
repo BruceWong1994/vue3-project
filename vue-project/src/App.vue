e9b679dfe6aa8a72130dd0cee3a3dacd80760e4e
767961652@qq.com <767961652@qq.com>
2025-07-05 11:19:23 +0800

    submit long time ago

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,26 +1,35 @@
 <template>
-  <TabPannel />
+  <!-- <TabPannel /> -->
+   <TabPannel />
+  <!-- <P1 /> -->
 </template>
 
 <script setup>
-  import TabPannel from './components/TabPannel/TapPannel.vue';
+// import TabPannel from './components/TabPannel/TapPannel.vue';
+import TabPannel from './components/CompositionApi/ClassStyle.vue';
+// import P1 from "./new/p1.vue";
 </script>
 
 <style>
-body{
-  margin:0;
-  padding:0;
-  background:#d3ecd3;
+body {
+  margin: 0;
+  padding: 0;
+  background: #d3ecd3;
 }
-h1,h2,h3,h4,h5,h6,p{
-  margin:0;
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
 }
 
-.outer{
-    border:1px solid red;
-    background:#b9e7b9;
-    padding:20px 10px;
-    margin-bottom:20px;
+.outer {
+  border: 1px solid red;
+  background: #b9e7b9;
+  padding: 20px 10px;
+  margin-bottom: 20px;
 }
 </style>
-
---
+++ vue-project/src/new/p1.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p>my name is {{ name }}, i'm {{ age }} years old</p>
+        <!-- <p>{{ info }}</p> -->
+         <div><button @click="addHandler">count:{{ count }}</button></div>
+         <div>这是第{{ count }}次点击</div>
+    </div>
+</template>
+
+<script>
+    //选项式api
+    export default{
+        data(){
+            return {
+                name:'Bruce',
+                age: 31,
+                count:0
+            }
+        },
+        // computed(){
+        //     return {
+        //         info:`my real name is${this.name}, i'm ${this.age} years old already`
+        //     }
+            
+        // },
+        methods:{
+            addHandler(){
+                this.count ++
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
